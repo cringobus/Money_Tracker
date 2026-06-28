f5228b2661ffbc6bb6c97ab1ffaf15decc76c624
cringobus <staschornij@gmail.com>
2026-06-28 23:55:36 +0300

    combined frontend and backend

---
+++ main (6).jsx
@@ -0,0 +1,16 @@
+﻿.h3{
+  color: #e1ffd7;
+}
+
+.calendar-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #679fe9;
+  border-radius: 20px;
+  padding: 14px 20px;
+  margin-top: 2vh;
+  width: 560px;
+  color: #2e4057;
+}
