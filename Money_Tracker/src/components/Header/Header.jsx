c8e2cb0d2f2702c2af5834f591c91a1186d25af6
i8or4yk <i8or4yk@gmail.com>
2026-06-28 22:24:07 +0300

    Add files via upload

--- Money_Tracker/src/components/Header/Header.jsx
+++ Money_Tracker/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     <div className="header">
       <img src="/proteus.png" className="MainLogo" />
       <h2 className='name'>Money Tracker</h2>
+      <a href="login.html" className="link">Log in/sign in</a>
     </div>
   );
 };
